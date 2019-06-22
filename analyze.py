d14f72c43504300c9b6140ea30297b2b073ac6e3
Pranav Salunke <pranav.salunke@gmail.com>
2019-06-21 23:19:28 -0700

    changed the html file's name depnding on the field and data filename.... .added parameter to save or not save the html files made by bokeh

--- analyze.py
+++ analyze.py
@@ -8,21 +8,25 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import os
 import pickle
-from bokeh.plotting import figure, output_file, show
+from bokeh.plotting import figure, output_file, show, save
+
 
 def creeatePickledName(filename, field):
     slashPos = filename.find("/")
-    slashPos = slashPos if slashPos !=-1 else 0 # if no slash, start at beggining
+    slashPos = slashPos if slashPos != -1 else 0  # if no slash, start at beggining
     dotPos = filename.find(".")
     pickleName = "pickledFigs/" + filename[slashPos:dotPos] + "__" + field + ".pickle"
     return pickleName
 
+
 def createPickledFig(pickleName, fig):
     pickle.dump(fig, open(pickleName, "wb"))
 
+
 def loadPickledFig(figname):
     fig = pickle.load(open(figname, 'rb'))
-    plt.show() 
+    plt.show()
+
 
 def cleanFile(filename):
     print("cleaning UnicodeEncodeError")
@@ -39,19 +43,20 @@ def cleanFile(filename):
             for line in oldreader:
                 line = [x.strip() for x in line]
                 if "UnicodeEncodeError" in line:
-                    print("remove UnicodeEncodeError: %s"%(line))
+                    print("remove UnicodeEncodeError: %s" % (line))
                     continue
 
                 if len(line) != numFields:
-                    print("remove incorrect line len: %s"%(line))
+                    print("remove incorrect line len: %s" % (line))
                     continue
 
                 newwriter.writerow(line)
 
     os.remove(tempFilename)
 
+
 def preAnalyze(filename):
-    print("doing Pre")
+    print("doing Preanalysis")
     count = 0
     badlines = []
     doAnalyze = True
@@ -77,7 +82,7 @@ def preAnalyze(filename):
     return doAnalyze
 
 
-def analyze(filename, field, showAllDates, plottingDate=True):
+def analyze(filename, field, showAllDates, plottingDate=True, saveHTML=False):
     print("starting analyze")
     showAllDates = showAllDates
 
@@ -95,7 +100,7 @@ def analyze(filename, field, showAllDates, plottingDate=True):
 
     print("building plot")
     startBuild = datetime.datetime.now()
-    
+
     fig, ax = plt.subplots()
 
     if plottingDate:
@@ -111,28 +116,37 @@ def analyze(filename, field, showAllDates, plottingDate=True):
             plt.xticks(ith.index, rotation=10)  # show every date
         else:
             fig.autofmt_xdate()  # format so its not too crowded
-        
+
         pn = creeatePickledName(filename, field)
         if not os.path.exists(pn):
             print("pickling fig")
-            createPickledFig(pn,fig)
+            createPickledFig(pn, fig)
 
         plt.show()
     else:
-        # bokeh is better for large data sets, not sure how to do time on it so use matplotlib 
-        if field=="Total Karma":
-            plot = figure(plot_width=800, plot_height=500,title="%s -- %s vs Counts" % (filename, field))
+        # bokeh is better for large data sets, not sure how to do time on it so use matplotlib
+        if field == "Total Karma":
+            plot = figure(plot_width=800, plot_height=500, title="%s -- %s vs Counts" % (filename, field))
         else:
             # categorical data
-            plot = figure(x_range=list(ith.index), plot_width=800, plot_height=500,title="%s -- %s vs Counts" % (filename, field))
-            
-        plot.circle(ith.index, ith.values, size=8, color="blue", alpha=0.8)
-        show(plot)
-        time.sleep(0.3) # let the rest load
+            plot = figure(x_range=list(ith.index), plot_width=800, plot_height=500, title="%s -- %s vs Counts" % (filename, field))
 
+        plot.circle(ith.index, ith.values, size=8, color="blue", alpha=0.8)
+        basefilename = filename[:filename.rfind(".")]  # ignore extention
+        htmlfilename = "analyze_%s_%s.html" % (basefilename, field)
+        output_file(htmlfilename)
+        show(plot)
+        time.sleep(0.3)  # let the rest load
+        if not saveHTML:  # just show it, dont save the html files
+            try:
+                os.remove(htmlfilename)  # remove the html file
+            except PermissionError:  # still loading
+                print("wait more")
+                time.sleep(0.5)  # wait another half second
+                os.remove(htmlfilename)  # try to remove the html file again
 
     print("  built plot")
-    print("    Build took: %s"%(str(datetime.datetime.now() - startBuild)))
+    print("    Build took: %s" % (str(datetime.datetime.now() - startBuild)))
 
 
 # Fields queried:
@@ -143,33 +157,36 @@ def analyze(filename, field, showAllDates, plottingDate=True):
 
 # filename = "small_popular_int.csv"  # small popular using interval
 # filename = "askreddit_2hInt.csv"  # 2h askreddit int
-# filename = "30min_popular_stream.csv" 
-# filename = "30min_popular_stream2.csv" 
-# filename = "30min_popular_5minInt.csv" 
+# filename = "30min_popular_stream.csv"
+# filename = "30min_popular_stream2.csv"
+# filename = "30min_popular_5minInt.csv"
 
-### long runs 
-# filename = "data/ucsc_2day12hour_interval5min.csv" 
-# filename = "data/ucsc_2day12hour_stream.csv" 
+# long runs
+# filename = "data/ucsc_2day12hour_interval5min.csv"
+# filename = "data/ucsc_2day12hour_stream.csv"
 
-# filename = "data/askreddit_2day12hour_interval5min.csv" 
-# filename = "data/askreddit_2day12hour_stream.csv" 
+# filename = "data/askreddit_2day12hour_interval5min.csv"
+# filename = "data/askreddit_2day12hour_stream.csv"
 
-filename = "data/Popular_2day12hour_interval5min.csv" 
-# filename = "data/popular_short_stream2.csv" 
+# filename = "data/Popular_2day12hour_interval5min.csv"
+# filename = "data/popular_short_stream2.csv"
 
-fields = ["Iteration Time (home)","Iteration Time (utc)","Post Time (utc)","Subreddit","Title","Author","Total Karma"]
+
+filename = "testingNewBreakpoint3_b.csv"
+
+fields = ["Iteration Time (home)", "Iteration Time (utc)", "Post Time (utc)", "Subreddit", "Title", "Author", "Total Karma"]
 # not plotting postid since that is unique anyway
 
 field = "Iteration Time (home)"
 # field = "Iteration Time (utc)"
 # field = "Post Time (utc)"
 # field = "Subreddit"
-# field = "Author" 
+# field = "Author"
 # field = "Title"
 # field = "Total Karma"
 # field = " Postid "
 plottingDate = "time" in field.lower()
-showAllDates = False # show all the collected date data if True, if False, let matplotlib create the scale
+showAllDates = False  # show all the collected date data if True, if False, let matplotlib create the scale
 
 print("for: %s\n" % (filename))
 
@@ -193,7 +210,7 @@ print("\nshowAllDates: %s" % (showAllDates))
 print("field: %s" % (field))
 print("plotting date: %s\n" % (str(plottingDate)))
 
-#load pickled figure (matplotlib)
+# load pickled figure (matplotlib)
 # pickedFigName = creeatePickledName("30min_popular_5minInt.csv", "Iteration Time (home)")
 # loadPickledFig(pickedFigName)
 
@@ -202,7 +219,7 @@ print("plotting date: %s\n" % (str(plottingDate)))
 for f in fields:
     if "stream" in filename and "Iteration" in f:
         print("SKIPPING === " + f.upper() + " === ")
-        continue # skip the iteration time for streams since they are all one time
+        continue  # skip the iteration time for streams since they are all one time
 
     print(" === " + f.upper() + " === ")
     p = "time" in f.lower()
